5ef94464a0c7b84450031b6de5cdb0a64e06c749
viditmathur <33325054+viditmathur@users.noreply.github.com>
2019-11-03 19:25:47 +0530

    /api mapping added

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
 
 
 const app = express();
-const port= process.env.PORT;
+const port= process.env.PORT||8080;
    
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 
+app.use('/api',routes);
 
 app.listen(port, () => {
     console.log('Server is up and running on port numner ' + port);
