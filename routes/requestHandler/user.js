fe5ac7b6474e2ac01fcbde4dec3ac01fb2d0514b
viditmathur <33325054+viditmathur@users.noreply.github.com>
2019-11-04 21:36:21 +0530

    jwt added

--- routes/requestHandler/user.js
+++ routes/requestHandler/user.js
@@ -1,6 +1,7 @@
 var express=require('express');
 var router=express.Router();
 var user=require('../../models/user');
+var jwt=require('jsonwebtoken');
 
 
 router.post('/signup', (req, res, next) => {
