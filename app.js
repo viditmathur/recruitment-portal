e89d7791ab834571566ee9da19336f52c3c19317
vidit <vidit.mathur@skilrock.com>
2019-11-04 12:07:39 +0530

    mongoose db connections added

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/routes');
+const mongoose = require('mongoose');
 
 
 const app = express();
 const port= process.env.PORT||8080;
-   
+
+const url = process.env.base+process.env.password+process.env.final;
+
+mongoose.connect(url, function(err, db) {
+    if (err) throw err;
+    console.log("Database connected!");
+  });
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
