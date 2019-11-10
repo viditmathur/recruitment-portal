667e33cb835fa1f4f0dc468829a0c9931cbe1802
viditmathur <33325054+viditmathur@users.noreply.github.com>
2019-11-10 15:02:15 +0530

    changes

--- models/user.js
+++ models/user.js
@@ -2,12 +2,7 @@ var mongoose=require('mongoose');
 var Schema=mongoose.Schema;
 var userSchema =new Schema({
 	
-    id:{
-        type:String,
-        unique:true,
-    },
-
-	email:{
+    email:{
 		type:String,
         unique:true,
         required:true
